9153f63c1ec4cd9be6da5482f8ebca35f5017146
sram02 <160508544+sram02@users.noreply.github.com>
2024-11-13 04:48:39 +0800

    Update Administrator.java
    Added change password feature

--- Hospital/src/hospital/Administrator.java
+++ Hospital/src/hospital/Administrator.java
@@ -1,5 +1,7 @@
 package hospital;
 
+import java.util.Scanner;
+
 public class Administrator extends Staff {
     private AdminInventoryManager inventoryManager;
 
@@ -10,5 +12,27 @@ public class Administrator extends Staff {
 
     public AdminInventoryManager getInventoryManager() {
         return inventoryManager;
+    }
+    
+    public void changePassword(Scanner scanner) {
+        System.out.print("Enter your current password: ");
+        String currentPassword = scanner.nextLine();
+
+        if (!this.getPassword().equals(currentPassword)) {
+            System.out.println("Incorrect current password.");
+            return;
+        }
+
+        System.out.print("Enter new password: ");
+        String newPassword = scanner.nextLine();
+        System.out.print("Confirm new password: ");
+        String confirmPassword = scanner.nextLine();
+
+        if (newPassword.equals(confirmPassword)) {
+            this.setPassword(newPassword);
+            System.out.println("Password changed successfully. \n");
+        } else {
+            System.out.println("Passwords do not match. Password change failed. \n");
+        }
     }
 }
