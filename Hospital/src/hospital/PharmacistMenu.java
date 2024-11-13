af4d7a9ca848fd9e06bd48a84940269c09119e21
sram02 <160508544+sram02@users.noreply.github.com>
2024-11-13 16:27:10 +0800

    Update PharmacistMenu.java

--- Hospital/src/hospital/PharmacistMenu.java
+++ Hospital/src/hospital/PharmacistMenu.java
@@ -1,17 +1,18 @@
 package hospital;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class PharmacistMenu {
     private Pharmacist pharmacist;
     private InventoryManager inventoryManager;
-    private AdminInventoryManager adminInventoryManager;  
+    private AdminInventoryManager adminInventoryManager;
     private Scanner scanner;
 
     public PharmacistMenu(Pharmacist pharmacist, InventoryManager inventoryManager, AdminInventoryManager adminInventoryManager) {
         this.pharmacist = pharmacist;
         this.inventoryManager = inventoryManager;
-        this.adminInventoryManager = adminInventoryManager;  // Initialize adminInventoryManager
+        this.adminInventoryManager = adminInventoryManager;
         this.scanner = new Scanner(System.in);
     }
 
@@ -23,7 +24,7 @@ public class PharmacistMenu {
             System.out.println("3. View Inventory");
             System.out.println("4. Submit Replenishment Request");
             System.out.println("5. Change Password");
-            System.out.println("6. Logout");
+            System.out.println("6. Logout \n");
             System.out.print("Enter your choice: ");
             int choice = scanner.nextInt();
             scanner.nextLine();  
@@ -41,13 +42,29 @@ public class PharmacistMenu {
                 case 4:
                     System.out.print("Enter medicine name: ");
                     String medicineName = scanner.nextLine();
+
+                    // Check if medicine exist in inventory
+                    if (inventoryManager.getInventory().getMedicineByName(medicineName) == null) {
+                        System.out.println("Invalid medicine. Please try again.\n");
+                        break;
+                    }
+
                     System.out.print("Enter requested quantity: ");
-                    int quantity = scanner.nextInt();
+                    int quantity;
+                    try {
+                        quantity = scanner.nextInt();
+                        scanner.nextLine();  
+                    } catch (InputMismatchException e) {
+                        System.out.println("Invalid quantity. Please enter a valid number.");
+                        scanner.nextLine(); 
+                        break;
+                    }
+
                     pharmacist.submitReplenishmentRequest(medicineName, quantity, adminInventoryManager);
                     break;
                 case 5:
                     pharmacist.changePassword(scanner);
-                    break;    
+                    break;
                 case 6:
                     System.out.println("Logging out...");
                     return;
