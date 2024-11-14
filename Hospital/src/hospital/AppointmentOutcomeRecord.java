20814ac0c094a20c16602a3a017f9ec4eee05dc8
NCHUA014 <167761412+NCHUA014@users.noreply.github.com>
2024-11-14 21:50:02 +0800

    Update AppointmentOutcomeRecord.java
    Nathan's Thursday update

--- Hospital/src/hospital/AppointmentOutcomeRecord.java
+++ Hospital/src/hospital/AppointmentOutcomeRecord.java
@@ -5,30 +5,21 @@ import java.util.Date;
 public class AppointmentOutcomeRecord {
     private String diagnosis;
     private String treatment;
-    private Date dateOfAppointment;
     private PrescribedMedication prescription;
-    private String bloodType;
-    private String patientName;
-    private String doctorName;
-    private int appointmentID;
 
-    public AppointmentOutcomeRecord(String diagnosis, String treatment, Date dateOfAppointment, PrescribedMedication prescription, String bloodType, String patientName, String doctorName, int appointmentID) {
+
+    public AppointmentOutcomeRecord(String diagnosis, String treatment) {
         this.diagnosis = diagnosis;
         this.treatment = treatment;
-        this.dateOfAppointment = dateOfAppointment;
-        this.prescription = prescription;
-        this.bloodType = bloodType;
-        this.patientName = patientName;
-        this.doctorName = doctorName;
-        this.appointmentID = appointmentID;
-    }
-
-    public void setStatus(String status) {
-        // Set prescription status or additional outcome updates as needed
+        this.prescription = null;
     }
 
     @Override
     public String toString() {
         return "AppointmentOutcomeRecord{" + "diagnosis='" + diagnosis + '\'' + ", treatment='" + treatment + '\'' + ", dateOfAppointment=" + dateOfAppointment + ", prescription=" + prescription + ", bloodType='" + bloodType + '\'' + ", patientName='" + patientName + '\'' + ", doctorName='" + doctorName + '\'' + ", appointmentID=" + appointmentID + '}';
+    }
+    
+    public void set_prescription(String name, int quantity) {
+    	this.prescription = new PrescribedMedication(name, quantity);
     }
 }
