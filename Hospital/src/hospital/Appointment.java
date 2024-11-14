623d463d518850ffd79bf26a675287430d6920bb
NCHUA014 <167761412+NCHUA014@users.noreply.github.com>
2024-11-14 21:47:26 +0800

    Update Appointment.java
    Nathan's thursday update

--- Hospital/src/hospital/Appointment.java
+++ Hospital/src/hospital/Appointment.java
@@ -3,16 +3,26 @@ package hospital;
 import java.util.Date;
 
 public class Appointment {
+	
+	public enum AppointmentStatus {
+		PENDING, CONFIRMED, CANCELLED, COMPLETED
+	}
+	
     private String patientID;
     private String doctorID;
     private Date date;
     private String time;
+    private AppointmentStatus status;
+    private AppointmentOutcomeRecord AppointmentOutcome;
 
+    
     public Appointment(String patientID, String doctorID, Date date, String time) {
         this.patientID = patientID;
         this.doctorID = doctorID;
         this.date = date;
         this.time = time;
+        this.status = AppointmentStatus.PENDING;
+        this.AppointmentOutcome = null;
     }
 
     // Getters and Setters
@@ -31,9 +41,26 @@ public class Appointment {
     public String getTime() {
         return time;
     }
+    
+    public AppointmentStatus getStatus() {
+    	return this.status;
+    }
+    
+    public void setStatus(AppointmentStatus status) {
+    	this.status = status;
+    }
 
     @Override
-    public String toString() {
-        return "Appointment with Doctor ID: " + doctorID + ", Date: " + date + ", Time: " + time;
+    public void toString() {
+        System.out.println("Appointment with Doctor ID: " + doctorID + ", Date: " + date + ", Time: " + time);
+        System.out.println("Patient ID: " + patientID);
+    }
+    
+    public void create_AOR(String diagnosis, String treatment) {
+    	this.AppointmentOutcome = new AppointmentOutcomeRecord(diagnosis, treatment);
+    }
+    
+    public AppointmentOutcomeRecord get_AOR() {
+    	return AppointmentOutcome;
     }
 }
