5cd87715f3337fdc56cb9514d8b986280f442651
NCHUA014 <167761412+NCHUA014@users.noreply.github.com>
2024-11-15 18:41:42 +0800

    Update DoctorMenu.java

--- Hospital/src/hospital/DoctorMenu.java
+++ Hospital/src/hospital/DoctorMenu.java
@@ -1,3 +1,4 @@
+
 package hospital;
 
 import java.util.Scanner;
@@ -19,14 +20,15 @@ public class DoctorMenu {
        
         while (true) {
             System.out.println("Welcome, Dr. " + doctor.getName() + "! This is the Doctor Menu.");
-            System.out.println("0. Change password");
+            System.out.println("0. Change password"); //ram's edit
             System.out.println("1. View Patient Medical Records under my care");
             System.out.println("2. Update Patient Medical Records under my care");
-            System.out.println("3. Set Availability for new appoinments");
-            System.out.println("4. View list of upcoming appointments");
-            System.out.println("5. Accept/Decline appointments");
-            System.out.println("6. Fill up completed appointment");
-            System.out.println("7. Logout");
+            System.out.println("3. View my personal schedule");
+            System.out.println("4. Set Availability for new appoinments");
+            System.out.println("5. View list of upcoming appointments");
+            System.out.println("6. Accept/Decline appointments");
+            System.out.println("7. Fill up completed appointment");
+            System.out.println("8. Logout");
             System.out.print("Enter your choice: ");
 
             int choice = scanner.nextInt();
@@ -42,31 +44,36 @@ public class DoctorMenu {
                 case 1:
                     // View blood type, past diagnoses, treatments.
                     System.out.println("Viewing patient records...");
-                    this.doctor.get_RM().viewPatientMedicalRecord();
+                    this.doctor.get_DRM().viewPatientMedicalRecord();
                     break;
                 case 2:
                     //updates the patient's past diagnoses and treatments.
                     System.out.println("Updating patient records...");
-                    this.doctor.get_RM().UpdatePatientRecord();
+                    this.doctor.get_DRM().UpdatePatientRecord();
                     break;
-                case 3:
+                    
+                case 3: 
+                	System.out.println("Viewing Availability...");
+                	this.doctor.get_DAM().View_Schedule();
+                	break;
+                case 4:
                     // Call method to set availability
                     System.out.println("Setting availability...");
-                    this.doctor.get_AM().schedule(this.doctor.getUserID());
+                    this.doctor.get_DAM().schedule(this.doctor.getUserID());
                     break;
-                case 4:
-                	System.out.println("View list of upcoming appointments...");
-                	this.doctor.get_AM().View_Upcoming_Appointments();
-                	break;
                 case 5:
-                	System.out.println("Accept/decline appointments...");
-                	this.doctor.get_AM().Accept_Appointments();
+                	System.out.println("View list of upcoming appointments...");
+                	this.doctor.get_DAM().View_Upcoming_Appointments();
                 	break;
                 case 6:
-                	System.out.println("Fill up completed appoinments...");
-                	this.doctor.get_AM().Fill_Completed_Appointment();
+                	System.out.println("Accept/decline appointments...");
+                	this.doctor.get_DAM().Accept_Appointments();
                 	break;
                 case 7:
+                	System.out.println("Fill up completed appoinments...");
+                	this.doctor.get_DAM().Fill_Completed_Appointment();
+                	break;
+                case 8:
                     System.out.println("Logging out...");
                     scanner.close();
                     return;  // Exit the menu and go back to the main login screen
