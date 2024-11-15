ef94701a4c5f97a3c7371272a9de2f717bd6910c
sram02 <160508544+sram02@users.noreply.github.com>
2024-11-15 22:56:06 +0800

    Update Doctor.java
    added changePassword method

--- Hospital/src/hospital/Doctor.java
+++ Hospital/src/hospital/Doctor.java
@@ -1,5 +1,7 @@
 package hospital;
 
+import java.util.Scanner;
+
 public class Doctor extends Staff {
 	
 	//attributes
@@ -12,6 +14,28 @@ public class Doctor extends Staff {
         super(userID, password, name, gender, age, StaffRole.DOCTOR); // Pass StaffRole.DOCTOR
         this.recordManager = new DRecordManager();
         this.appointmentManager = new DAppointmentManager();
+    }
+    
+    public void changePassword(Scanner scanner) {
+        System.out.print("Enter your current password: ");
+        String currentPassword = scanner.nextLine();
+
+        if (!this.getPassword().equals(currentPassword)) {
+            System.out.println("Incorrect current password.");
+            return;
+        }
+
+        System.out.print("Enter new password: ");
+        String newPassword = scanner.nextLine();
+        System.out.print("Confirm new password: ");
+        String confirmPassword = scanner.nextLine();
+
+        if (newPassword.equals(confirmPassword)) {
+            this.setPassword(newPassword);
+            System.out.println("Password changed successfully. \n");
+        } else {
+            System.out.println("Passwords do not match. Password change failed. \n");
+        }
     }
     
     public DRecordManager get_DRM() {
