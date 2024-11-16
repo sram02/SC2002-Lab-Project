8bdbe3dc442ff856e2216fa88b0de0df9f223be9
NCHUA014 <167761412+NCHUA014@users.noreply.github.com>
2024-11-16 19:24:14 +0800

    Update Doctor.java

--- Hospital/src/hospital/Doctor.java
+++ Hospital/src/hospital/Doctor.java
@@ -1,48 +1,24 @@
 package hospital;
 
-import java.util.Scanner;
-
 public class Doctor extends Staff {
 	
-	//attributes
-    private DRecordManager recordManager;
-    private DAppointmentManager appointmentManager;
+	// Attributes
+    private final DRecordManager recordManager; // Personal patient records
+    private final DAppointmentManager appointmentManager; // Personal schedules
     
-    
-    //constructor
+    // Constructor
     public Doctor(String userID, String password, String name, String gender, int age) {
         super(userID, password, name, gender, age, StaffRole.DOCTOR); // Pass StaffRole.DOCTOR
         this.recordManager = new DRecordManager();
         this.appointmentManager = new DAppointmentManager();
     }
-    
-    public void changePassword(Scanner scanner) {
-        System.out.print("Enter your current password: ");
-        String currentPassword = scanner.nextLine();
 
-        if (!this.getPassword().equals(currentPassword)) {
-            System.out.println("Incorrect current password.");
-            return;
-        }
-
-        System.out.print("Enter new password: ");
-        String newPassword = scanner.nextLine();
-        System.out.print("Confirm new password: ");
-        String confirmPassword = scanner.nextLine();
-
-        if (newPassword.equals(confirmPassword)) {
-            this.setPassword(newPassword);
-            System.out.println("Password changed successfully. \n");
-        } else {
-            System.out.println("Passwords do not match. Password change failed. \n");
-        }
-    }
-    
-    public DRecordManager get_DRM() {
+    public DRecordManager getRecordManager() {
     	return recordManager;
     }
-    
-    public DAppointmentManager get_DAM() {
+
+    public DAppointmentManager getAppointmentManager() {
     	return appointmentManager;
     }   
+    
 }
